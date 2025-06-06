801dfce752a11d6fd2280a78b56a94bcf031e54e
Prathmesh <mailyourprathmesh@gmail.com>
2025-06-06 07:16:33 +0530

    Added MainRoute file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,16 @@ import { useEffect } from "react"
 import axios from "./api/axiosConfig"
 import { asyncGetUsers } from "../store/userActions"
 import {useDispatch, useSelector} from "react-redux"
+import Mainroutes from "./routes/Mainroutes"
 
 const App = () => {
-    const data = useSelector((state) => state)
-    const dispatch = useDispatch();
-
-    console.log(data)
+    
+    return (<div className="text-white font-thin w-screen h-screen bg-blue-900">
         
-    useEffect(() => {
-        dispatch(asyncGetUsers());
-    }, [])
-    return <div>Chaman</div>
+        <Mainroutes>
+            
+        </Mainroutes>
+        </div>);
 }
 
 export default App;
